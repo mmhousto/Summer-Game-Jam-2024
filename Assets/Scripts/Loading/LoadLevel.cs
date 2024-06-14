da94a1863d645941e6ce12e4414e6f81c5643150
Morgan Houston <mmhouston6@live.com>
2024-06-14 09:56:11 -0500

    Scene Management

---
+++ Assets/Scripts/Loading/LoadLevel.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    #region Fields
+    [Tooltip("Player Sprite that follows loading bar.")]
+    public GameObject player;
+    [Tooltip("The end of the loading bar.")]
+    public GameObject loadingBarEnd;
+    [Tooltip("The loading bar.")]
+    public Slider loadingBar;
+    private float progress;
+
+    #endregion
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncScene(SceneLoader.levelToLoad));
+    }
+
+    #endregion
+
+    #region Methods
+
+    IEnumerator LoadAsyncScene(int index)
+    {
+        yield return null;
+
+        AsyncOperation loading = SceneManager.LoadSceneAsync(index);
+
+        while(!loading.isDone)
+        {
+            progress = Mathf.Clamp(loading.progress / .9f, 0f, 0.9999f);
+
+            loadingBar.value = progress * 100f;
+            player.transform.position = loadingBarEnd.transform.position;
+
+            yield return null;
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Loading/Loading.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    [Tooltip("The level to be loaded.")]
+    public int levelToLoad;
+
+    /// <summary>
+    /// Once player enters trigger area load loading scene and level to load.
+    /// </summary>
+    /// <param name="other">The object that triggered</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            SceneLoader.LoadLevel(levelToLoad);
+    }
+
+}
